ad032fcaf1bad7b0dc2bc4e89f2d6d687db6c0fc
PanchV <32369823+PanchV@users.noreply.github.com>
2022-03-07 01:21:12 -0500

    Major changes

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -10,31 +10,31 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/CI-CD">CI:CD</a>' in response.data
     #assert b'<a class="nav-link" href="/page4">Page 4</a>' in response.data
 
-def test_request_index(client):
+def test_request_Home(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
     assert b"Home" in response.data
 
-def test_request_about(client):
+def test_request_Git(client):
     """This makes the index page"""
     response = client.get("/Git")
     assert response.status_code == 200
     assert b"Git" in response.data
 
-def test_request_page1(client):
+def test_request_Docker(client):
     """This makes the index page"""
     response = client.get("/Docker")
     assert response.status_code == 200
     assert b"Docker" in response.data
 
-def test_request_page2(client):
+def test_request_PythonFlask(client):
     """This makes the index page"""
     response = client.get("/Python-Flask")
     assert response.status_code == 200
     assert b"Python-Flask" in response.data
 
-def test_request_page3(client):
+def test_request_CICD(client):
     """This makes the index page"""
     response = client.get("/CI-CD")
     assert response.status_code == 200
